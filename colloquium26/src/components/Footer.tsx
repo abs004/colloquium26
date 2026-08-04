5692015a9448cc1f5b2b939a40ee13530af0a949
Abhishek <abhishek2004abs@gmail.com>
2026-08-05 00:16:23 +0530

    added glow

--- colloquium26/src/components/Footer.tsx
+++ colloquium26/src/components/Footer.tsx
@@ -9,7 +9,7 @@ import { InstagramIcon, XIcon, LinkedinIcon } from "./icons/SocialIcons";
 
 export default function Footer() {
   return (
-    <footer className="relative border-t border-white/10 px-6 py-12 sm:py-16">
+    <footer className="relative px-6 py-12 sm:py-16">
       <div className="mx-auto max-w-6xl">
         <button
           onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
